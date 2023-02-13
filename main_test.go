b325f16f3016448aa3cf3e059d30216d15caa862
Andres Cruz <mail@acio.dev>
2023-02-13 15:29:22 -0800

    Add additional benchmarks

--- main_test.go
+++ main_test.go
@@ -6,9 +6,10 @@ import (
 )
 
 var (
-	sizeFixed          = 5
+	sizeFixed          = 25
 	sizeCustom         = 4
 	gridFixed          = loadSample(sizeFixed, math.MaxInt32)
+	gridFixed1Indexed  = convertTo1IndexBased(sizeFixed, gridFixed)
 	gridCustom0Indexed = [][]int{
 		{-1, 10, 15, 20},
 		{10, -1, 35, 25},
@@ -24,14 +25,20 @@ func Benchmark_TSP_GreedyFixed(b *testing.B) {
 	}
 }
 
-func Benchmark_TSP_GreedyCustom(b *testing.B) {
+func Benchmark_TSP_DPFixed(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		tspGreedy(sizeCustom, gridCustom0Indexed)
+		tspDynamicProgramming(sizeFixed, gridFixed1Indexed)
 	}
 }
 
-func Benchmark_TSP_DPCustom(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		tspDynamicProgramming(sizeCustom, gridCustom1Indexed)
-	}
-}
+//func Benchmark_TSP_GreedyCustom(b *testing.B) {
+//	for i := 0; i < b.N; i++ {
+//		tspGreedy(sizeCustom, gridCustom0Indexed)
+//	}
+//}
+//
+//func Benchmark_TSP_DPCustom(b *testing.B) {
+//	for i := 0; i < b.N; i++ {
+//		tspDynamicProgramming(sizeCustom, gridCustom1Indexed)
+//	}
+//}
