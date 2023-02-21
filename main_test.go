9b2e7ddce68538f3496bf9a1f8b17ead42ec1bc5
Andres Cruz <mail@acio.dev>
2023-02-21 15:19:53 -0800

    Add new test cases for n=11 to n=20. Remove old sample files.

--- main_test.go
+++ main_test.go
@@ -8,30 +8,24 @@ import (
 
 var (
 	inputsZeroIndexed = map[int][][]int{
-		5:   loadSample(5, math.MaxInt32, false),
-		6:   loadSample(6, math.MaxInt32, false),
-		7:   loadSample(7, math.MaxInt32, false),
-		8:   loadSample(8, math.MaxInt32, false),
-		9:   loadSample(9, math.MaxInt32, false),
-		10:  loadSample(10, math.MaxInt32, false),
-		25:  loadSample(25, math.MaxInt32, false),
-		50:  loadSample(50, math.MaxInt32, false),
-		100: loadSample(100, math.MaxInt32, false),
+		5:  loadSample(5, math.MaxInt32, false),
+		6:  loadSample(6, math.MaxInt32, false),
+		7:  loadSample(7, math.MaxInt32, false),
+		8:  loadSample(8, math.MaxInt32, false),
+		9:  loadSample(9, math.MaxInt32, false),
+		10: loadSample(10, math.MaxInt32, false),
 	}
 
 	inputs1Indexed = map[int][][]int{
-		5:   loadSample(5, 0, true),
-		6:   loadSample(6, 0, true),
-		7:   loadSample(7, 0, true),
-		8:   loadSample(8, 0, true),
-		9:   loadSample(9, 0, true),
-		10:  loadSample(10, 0, true),
-		25:  loadSample(25, 0, true),
-		50:  loadSample(50, 0, true),
-		100: loadSample(100, 0, true),
+		5:  loadSample(5, 0, true),
+		6:  loadSample(6, 0, true),
+		7:  loadSample(7, 0, true),
+		8:  loadSample(8, 0, true),
+		9:  loadSample(9, 0, true),
+		10: loadSample(10, 0, true),
 	}
 
-	testCases = []int{5, 6, 7, 8, 9, 10, 25, 50, 100}
+	testCases = []int{5, 6, 7, 8, 9, 10}
 )
 
 func BenchmarkGreedy(b *testing.B) {
