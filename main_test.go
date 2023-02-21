b7b2cbeecf67962bec9306ab2f47836e6a197a50
Andres Cruz <mail@acio.dev>
2023-02-21 15:38:12 -0800

    Isolate input(s) and reset timer to reduce variance

--- main_test.go
+++ main_test.go
@@ -7,28 +7,13 @@ import (
 )
 
 var (
-	inputsZeroIndexed = map[int][][]int{
-		5:  loadSample(5, math.MaxInt32, false),
-		6:  loadSample(6, math.MaxInt32, false),
-		7:  loadSample(7, math.MaxInt32, false),
-		8:  loadSample(8, math.MaxInt32, false),
-		9:  loadSample(9, math.MaxInt32, false),
-		10: loadSample(10, math.MaxInt32, false),
-	}
-
-	inputs1Indexed = map[int][][]int{
-		5:  loadSample(5, 0, true),
-		6:  loadSample(6, 0, true),
-		7:  loadSample(7, 0, true),
-		8:  loadSample(8, 0, true),
-		9:  loadSample(9, 0, true),
-		10: loadSample(10, 0, true),
-	}
-
 	testCases = []int{5, 6, 7, 8, 9, 10}
 )
 
 func BenchmarkGreedy(b *testing.B) {
+	inputsZeroIndexed := createZeroIndexedInput()
+	b.ResetTimer()
+
 	for _, v := range testCases {
 		b.Run(fmt.Sprintf("n_%d", v), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
@@ -39,11 +24,32 @@ func BenchmarkGreedy(b *testing.B) {
 }
 
 func BenchmarkDynamicProgramming(b *testing.B) {
+	inputsOneIndexed := createOneIndexedInput()
+	b.ResetTimer()
+
 	for _, v := range testCases {
 		b.Run(fmt.Sprintf("n_%d", v), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				tspDynamicProgramming(v, inputs1Indexed[v])
+				tspDynamicProgramming(v, inputsOneIndexed[v])
 			}
 		})
 	}
+}
+
+// Helper function to create isolated data (zero indexed)
+func createZeroIndexedInput() map[int][][]int {
+	inputsZeroIndexed := make(map[int][][]int)
+	for _, testCase := range testCases {
+		inputsZeroIndexed[testCase] = loadSample(testCase, math.MaxInt32, false)
+	}
+	return inputsZeroIndexed
+}
+
+// Helper function to create isolated data (zero indexed)
+func createOneIndexedInput() map[int][][]int {
+	inputsOneIndexed := make(map[int][][]int)
+	for _, testCase := range testCases {
+		inputsOneIndexed[testCase] = loadSample(testCase, 0, true)
+	}
+	return inputsOneIndexed
 }
